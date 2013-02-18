3bb1d1335aa61d44efdd157a6e9973864d3e1932
Nikolay Safin <safin@aboutecho.com>
2013-02-18 15:56:44 +0400

    Added the ability of loading configs from S3 (wip)

--- src/loader.js
+++ src/loader.js
@@ -14,7 +14,8 @@ Echo.Loader = {
 	"debug": false,
 	"config": {
 		"cdnBaseURL": "http://cdn.echoenabled.com/",
-		"errorTimeout": 5000 // 5 sec
+		"errorTimeout": 5000, // 5 sec
+		"storageURL": "https://s3.amazonaws.com/echo-canvases/"
 	},
 	"overrides": {},
 	"vars": {
@@ -389,18 +390,27 @@ Echo.Loader._initCanvases = function(canvases) {
 		return;
 	}
 
-	Echo.Loader._fetchCanvasConfigs(collection, function(configs) {
-		Echo.jQuery.each(collection, function(id, canvas) {
-			var config = configs[canvas.id];
+	Echo.jQuery.map(collection, function(canvas) {
+		Echo.Loader._fetchCanvasConfig(canvas.id, function(config) {
 			if (!config) return;
-
-			// copy config object to prevent the same object
-			// sharing across multiple canvas instances
-			canvas.config = Echo.jQuery.extend(true, {}, configs[canvas.id]);
-
+			canvas.config = config;
 			Echo.Loader._initCanvas(canvas);
-		});
+		})
 	});
+};
+
+Echo.Loader._fetchCanvasConfig = function(canvasId, callback) {
+	Echo.jQuery.get(Echo.Loader.config.storageURL + canvasId, function(response) {
+		if (!response) {
+			Echo.Loader._error({
+				"args": response,
+				"code": "unable_to_retrieve_app_config",
+				"message": "Unable to retrieve Canvas configs from AppServer"
+			});
+			return;
+		}
+		callback(response);
+	}, "json");
 };
 
 Echo.Loader._initCanvas = function(canvas) {
@@ -493,61 +503,6 @@ Echo.Loader._getAppScriptURL = function(config) {
 Echo.Loader._createApplicationTarget = function(config) {
 	// TODO: add more specific classname and may be id from canvas?
 	return Echo.jQuery('<div class="echo-application"></div>');
-};
-
-Echo.Loader._fetchCanvasConfigs = function(canvases, callback) {
-	if (!canvases.length) return callback();
-	var processed = {};
-	var requests = Echo.Utils.foldl([], canvases, function(canvas, acc) {
-
-		// do not request config twice
-		if (processed[canvas.id]) return;
-
-		processed[canvas.id] = true;
-		acc.push({
-			"id": canvas.id,
-			"key": canvas.id,
-			"public": true,
-			"method": "kvs/get"
-		});
-	});
-	Echo.jQuery.get("http://api.echoenabled.com/v1/mux", {
-		"appkey": canvases[0].appkey,
-		"requests": Echo.Utils.objectToJSON(requests)
-	}, function(response) {
-		if (!response || response.result === "error") {
-			Echo.Loader._error({
-				"args": response,
-				"code": "unable_to_retrieve_app_config",
-				"message": "Unable to retrieve Canvas configs from AppServer"
-			});
-			return;
-		}
-		var configs = Echo.Utils.foldl({}, response, function(data, acc, id) {
-			if (!data || data.result === "error") {
-				Echo.Loader._error({
-					"args": data,
-					"code": "unable_to_retrieve_app_config",
-					"message": "Unable to retrieve Canvas config from AppServer"
-				});
-				return;
-			}
-			var config;
-			try {
-				config = Echo.jQuery.parseJSON(data.value);
-			} catch(exception) {
-				Echo.Loader._error({
-					"args": [data, exception],
-					"code": "unable_to_parse_app_config",
-					"message": "Unable to parse JSON config"
-				});
-			}
-			if (config) {
-				acc[id] = config;
-			}
-		});
-		callback(configs);
-	}, "jsonp");
 };
 
 Echo.Loader._error = function(data) {
