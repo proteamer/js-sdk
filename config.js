eff046b9dd2fa3f968cd5062dfd1ada5108efb86
Nikolay Safin <safin@aboutecho.com>
2013-05-04 02:08:56 +0400

    Fixed travis config

--- config.js
+++ config.js
@@ -1,12 +1,12 @@
 var config = {
     "browserstack": {
-        "username": process.env.browserstack.username,
-        "password": process.env.browserstack.password,
+        "username": process.env.bs_username,
+        "password": process.env.bs_password,
         "timeout": 480
     },
     "port": 9000,
     "tunnellink": "localhost",
-    "tunnel": "java -jar BrowserStackTunnel.jar " + process.env.browserstack.token + " localhost,<port>,0"
+    "tunnel": "java -jar BrowserStackTunnel.jar " + process.env.bs_token + " localhost,<port>,0"
 };
 
 module.exports = config;
